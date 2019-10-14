b50670100befc9ed1edf4200edc7be15eafcc499
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-10-14 18:15:40 +0200

    turns out im fucking retarded

--- src/admin.rs
+++ src/admin.rs
@@ -1,16 +1,12 @@
-pub use admin::*;
 
-mod admin{
-    
-    use rocket::FromRequest;
+use rocket::FromRequest;
 
-    pub struct AdminGuard;
+pub struct AdminGuard;
 
-    impl FromRequest for AdminGuard{
-        type Error = String;
-        fn from_request(){
-            unimplemented!();
-        }
+impl FromRequest for AdminGuard{
+    type Error = String;
+    fn from_request(){
+        unimplemented!();
     }
-
 }
+
--- src/login.rs
+++ src/login.rs
@@ -1,44 +1,39 @@
 
-pub use login::*;
+use rocket::data::FromDataSimple;
+use rocket::data::Outcome;
+use rocket::{Data, Outcome::*, Request};
+use rocket::http::{ContentType, Status};
+use std::io::Read;
 
-mod login {
+const LIMIT: u64 = 256; // input data limit
 
-    use rocket::data::FromDataSimple;
-    use rocket::data::Outcome;
-    use rocket::{Data, Outcome::*, Request};
-    use rocket::http::{ContentType, Status};
-    use std::io::Read;
+pub struct Login {
+    login: String,
+    password: String,
+}
 
-    const LIMIT: u64 = 256; // input data limit
+impl FromDataSimple for Login {
+    type Error = String;
 
-    pub struct Login {
-        login: String,
-        password: String,
-    }
-
-    impl FromDataSimple for Login {
-        type Error = String;
-
-        fn from_data(req: &Request, data: Data) -> Outcome<Self, String> {
-            let person_ct = ContentType::new("application", "x-login");
-            if req.content_type() != Some(&person_ct) {
-                return Outcome::Forward(data);
-            }
-
-            let mut string = String::new();
-            if let Err(e) = data.open().take(LIMIT).read_to_string(&mut string) {
-                return Failure((Status::InternalServerError, format!("{:?}", e)));
-            }
-
-            let (login, password) = match string.find(':') {
-                Some(i) => (string[..i].to_string(), &string[(i + 1)..]),
-                None => return Failure((Status::UnprocessableEntity, "':'".into())),
-            };
-
-            Success(Login {
-                login: login.to_string(),
-                password: password.to_string(),
-            })
+    fn from_data(req: &Request, data: Data) -> Outcome<Self, String> {
+        let person_ct = ContentType::new("application", "x-login");
+        if req.content_type() != Some(&person_ct) {
+            return Outcome::Forward(data);
         }
+
+        let mut string = String::new();
+        if let Err(e) = data.open().take(LIMIT).read_to_string(&mut string) {
+            return Failure((Status::InternalServerError, format!("{:?}", e)));
+        }
+
+        let (login, password) = match string.find(':') {
+            Some(i) => (string[..i].to_string(), &string[(i + 1)..]),
+            None => return Failure((Status::UnprocessableEntity, "':'".into())),
+        };
+
+        Success(Login {
+            login: login.to_string(),
+            password: password.to_string(),
+        })
     }
 }
