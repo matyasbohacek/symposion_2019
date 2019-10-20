f3ca526dad032a8740414b8940d6c98794bde10f
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-10-21 00:18:50 +0200

    compiles again

--- src/admin.rs
+++ src/admin.rs
@@ -6,10 +6,11 @@ pub struct AdminGuard(String);
 impl<'a, 'r> FromRequest<'a, 'r> for AdminGuard{
     type Error = String;
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error>{
-        let login = request.cookies()
+        let login: String = request.cookies()
             .get_private("login")
             .unwrap_or(Cookie::new("login", "none"))
-            .value();
+            .value()
+            .to_string();
         request::Outcome::Success(AdminGuard(login.to_string()))
     }
 }
