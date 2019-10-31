bbe886eb2f557413ca2e84393fbf66561d129135
Lukáš Hozda <luk.hozda@gmail.com>
2019-10-31 22:02:58 +0100

    better static server

--- src/static_server.rs
+++ src/static_server.rs
@@ -6,18 +6,18 @@ pub(crate) fn index() -> NamedFile {
     NamedFile::open("www/index.html").unwrap()
 }
 
-#[get("/style/<file>")]
-pub(crate) fn styling(file: String) -> Option<NamedFile> {
-    NamedFile::open(format!("style/{}", file)).ok()
+#[get("/style/<file..>")]
+pub(crate) fn styling(file: PathBuf) -> Option<NamedFile> {
+	NamedFile::open(Path::new("style/").join(file)).ok()
 }
 
-#[get("/static/<file>")]
-pub(crate) fn static_files(file: String) -> Option<NamedFile> {
-    NamedFile::open(format!("static/{}", file)).ok()
+#[get("/static/<file..>")]
+pub(crate) fn static_files(file: PathBuf) -> Option<NamedFile> {
+	NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
 #[get("/pkg/<file..>", rank = 0)]
-pub fn wasm_styling(file: PathBuf) -> Option<NamedFile> {
+pub(crate) fn wasm_styling(file: PathBuf) -> Option<NamedFile> {
 	NamedFile::open(Path::new("pkg/").join(file)).ok()
 }
 
