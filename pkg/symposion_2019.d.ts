09e1967b4574ec79e64e6a7b6bc4b094d60e1341
Lukáš Hozda <luk.hozda@gmail.com>
2019-10-31 22:20:05 +0100

    unholy

---
+++ pkg/symposion_2019.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/**
+* @returns {any} 
+*/
+export function harmonogram_json(): any;
+/**
+*/
+export function start(): void;
+/**
+*/
+export class Harmonogram {
+  free(): void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {RequestInfo | BufferSource | WebAssembly.Module} module_or_path
+*
+* @returns {Promise<any>}
+*/
+export default function init (module_or_path?: RequestInfo | BufferSource | WebAssembly.Module): Promise<any>;
+        
